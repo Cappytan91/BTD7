6c81844bc6ba6c07923689543916ff37048b9765
STREAM-PC <cappytan91@gmail.com>
2022-06-12 10:57:18 -0500

    Almost done with episode 39

--- src/com/jason/btd7/MainMenu.java
+++ src/com/jason/btd7/MainMenu.java
@@ -1,6 +1,8 @@
 package com.jason.btd7;
 
 import com.jason.btd7.UI.*;
+import com.jason.btd7.helpers.StateManager;
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.opengl.Texture;
 
 import static com.jason.btd7.helpers.Artist.*;
@@ -18,9 +20,18 @@ public class MainMenu {
         menuUI.addButton("quit", "quitbutton", WIDTH * 2 / 3 - 128, (int) (HEIGHT * 0.74f));
     }
 
+    public void updateButtons(){
+        if(Mouse.isButtonDown(0)){
+            if(menuUI.isButtonClicked("Play")){
+                StateManager.setState(StateManager.GameState.GAME);
+            }
+        }
+    }
+
     public void update(){
         DrawQuadTex(background, 0, 0, 2048, 1024);
         menuUI.draw();
+        updateButtons();
     }
 
 }
