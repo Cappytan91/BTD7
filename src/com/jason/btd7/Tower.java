e71de234c2024a5e5e4478f79f09db70f8af88cf
STREAM-PC <cappytan91@gmail.com>
2022-06-17 21:13:51 -0500

    just random stuff + normal projectile

--- src/com/jason/btd7/Tower.java
+++ src/com/jason/btd7/Tower.java
@@ -1,6 +1,7 @@
 package com.jason.btd7;
 
 
+import com.jason.btd7.Projectiles.ProjectileBullet;
 import com.jason.btd7.Projectiles.ProjectileIceBall;
 import org.newdawn.slick.opengl.Texture;
 
@@ -73,7 +74,7 @@ public abstract class Tower implements Entity{
 
     public void shoot(){
         timeSinceLastShot = 0;
-        projectiles.add(new ProjectileIceBall(QuickLoad("iceBullet"), target, x + TILE_SIZE / 2 - TILE_SIZE / 4, y + TILE_SIZE / 2 - TILE_SIZE / 4, 32, 32, 1000, damage));
+        projectiles.add(new ProjectileBullet(QuickLoad("bullet"), target, x + TILE_SIZE / 2 - TILE_SIZE / 4, y + TILE_SIZE / 2 - TILE_SIZE / 4, 32, 32, 1000, damage));
     }
 
     public void updateEnemyList(ArrayList<Enemy> newList){
@@ -146,5 +147,15 @@ public abstract class Tower implements Entity{
         this.timeSinceLastShot = time;
     }
 
+    public ArrayList<Projectile> getProjectiles() {
+        return projectiles;
+    }
 
+    public void setProjectiles(ArrayList<Projectile> projectiles) {
+        this.projectiles = projectiles;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
--- src/com/jason/btd7/Towers/TowerIce.java
+++ src/com/jason/btd7/Towers/TowerIce.java
@@ -1,11 +1,15 @@
 package com.jason.btd7.Towers;
 
 import com.jason.btd7.Enemy;
+import com.jason.btd7.Projectiles.ProjectileIceBall;
 import com.jason.btd7.Tile;
 import com.jason.btd7.Tower;
 import com.jason.btd7.TowerType;
 
 import java.util.ArrayList;
+
+import static com.jason.btd7.helpers.Artist.QuickLoad;
+import static com.jason.btd7.helpers.Artist.TILE_SIZE;
 
 public class TowerIce extends Tower {
 
@@ -13,5 +17,9 @@ public class TowerIce extends Tower {
         super(type, startTile, enemies);
     }
 
-
+    @Override
+    public void shoot(){
+        setTimeSinceLastShot(0);
+        getProjectiles().add(new ProjectileIceBall(QuickLoad("iceBullet"), getTarget(), getX() + TILE_SIZE / 2 - TILE_SIZE / 4, getY() + TILE_SIZE / 2 - TILE_SIZE / 4, 32, 32, 1000, getDamage()));
+    }
 }
