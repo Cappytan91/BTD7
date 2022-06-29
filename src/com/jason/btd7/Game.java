bbf715d72b9f275c71648b574f192d3af014ef34
Cappytan91 <cappytan91@gmail.com>
2022-06-29 10:21:32 -0500

    episode 63 done!! (added custom Menus)

--- src/com/jason/btd7/Game.java
+++ src/com/jason/btd7/Game.java
@@ -2,6 +2,7 @@ package com.jason.btd7;
 
 import com.jason.btd7.Towers.TowerCannonBlue;
 import com.jason.btd7.Towers.TowerCannonIce;
+import com.jason.btd7.UI.Button;
 import com.jason.btd7.UI.UI;
 import org.lwjgl.input.Mouse;
 
@@ -29,8 +30,11 @@ public class Game {
 
     private void setupUI(){
         towerPickerUI = new UI();
-        towerPickerUI.addButton("CannonIce", "cannonIceGun", 0, 0);
-        towerPickerUI.addButton("CannonBlue", "cannonGunBlue", 64, 0);
+        //towerPickerUI.addButton("CannonIce", "cannonIceGun", 0, 0);
+        //towerPickerUI.addButton("CannonBlue", "cannonGunBlue", 64, 0);
+        towerPickerUI.createMenu("TowerPicker", 0, 0);
+        towerPickerUI.getMenu("TowerPicker").addButton(new Button("CannonIce", QuickLoad("cannonIceGun"), 0, 0));
+        towerPickerUI.getMenu("TowerPicker").addButton(new Button("CannonBlue", QuickLoad("cannonGunBlue"), 0, 0));
     }
 
     private void updateUI(){
@@ -40,9 +44,9 @@ public class Game {
             boolean mouseClicked = Mouse.isButtonDown(0);
 
             if (mouseClicked && !leftMouseButtonDown) {
-                if (towerPickerUI.isButtonClicked("CannonIce"))
+                if (towerPickerUI.getMenu("TowerPicker").isButtonClicked("CannonIce"))
                     player.pickTower(new TowerCannonIce(TowerType.CannonIce, grid.getTile(0, 0), waveManager.getCurrentWave().getEnemyList()));
-                if (towerPickerUI.isButtonClicked("CannonBlue"))
+                if (towerPickerUI.getMenu("TowerPicker").isButtonClicked("CannonBlue"))
                     player.pickTower(new TowerCannonBlue(TowerType.CannonBlue, grid.getTile(0, 0), waveManager.getCurrentWave().getEnemyList()));
             }
         }
